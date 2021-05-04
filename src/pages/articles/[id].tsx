073758ecc5377f5bebbebb18c0fa1803753f08c6
Crea7dosSantos <cr26yusaku0720@icloud.com>
2021-05-05 00:56:51 +0900

    close #5

---
+++ src/pages/articles/[id].tsx
@@ -0,0 +1,34 @@
+import { getArticles, getArticleById } from "../../../service/article"
+
+export default function articleDetail({ article }) {
+    return (
+        <div>
+            <h1>{article.title}</h1>
+            <div>
+                <p>記事作成日時: {article.created_at}</p>
+                <p>{article.body}</p>
+            </div>
+        </div>
+    )
+}
+
+export const getStaticPaths = async () => {
+    const res = await getArticles()
+    const repos = await res.json()
+
+    const paths = repos.contents.map(repo => `/articles/${repo.id}`)
+    return { paths, fallback: false }
+}
+
+export const getStaticProps = async contents => {
+    const id = contents.params.id
+
+    const res = await getArticleById(id)
+    const article = await res.json()
+
+    return {
+        props: {
+            article: article
+        }
+    }
+}
