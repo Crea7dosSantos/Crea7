8eee4922416f0ea2a7f9da1b9784dfae425f7e28
Crea7dosSantos <cr26yusaku0720@icloud.com>
2021-05-21 01:21:54 +0900

    tmp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,10 @@ import { getArticles } from '../service/article'
 export default function Home({ articles }) {
   return (
     <div>
-      <h2>最新の記事</h2>
+      <h2 className='text-7xl text-red-500 font-bold' >最新の記事</h2>
       <div>
         {articles.map(article => (
+
           <React.Fragment key={article.id}>
             <Link href='/articles/[:id]' as={`/articles/${article.id}`}>
               <a>
@@ -25,6 +26,7 @@ export default function Home({ articles }) {
         ))}
       </div>
     </div>
+
   )
 }
 
@@ -38,3 +40,18 @@ export const getStaticProps = async () => {
     }
   }
 }
+
+{/* <div class="max-w-sm rounded overflow-hidden shadow-lg">
+  <img class="w-full" src={article.topImage.url} alt="Sunset in the mountains"/>
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div class="px-6 py-4">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+  </div>
+</div> */}
