bc55c290721f3a140aafc288e7bd51702792c5b1
Crea7dosSantos <cr26yusaku0720@icloud.com>
2021-05-29 22:42:08 +0900

    close #31

--- src/service/article.ts
+++ src/service/article.ts
@@ -5,5 +5,5 @@ const key = {
 export const getArticles = async () =>
   await fetch('https://c7ds-log.microcms.io/api/v1/articles', key)
 
-export const getArticleById = async (id) =>
+export const getArticleById = async (id: number) =>
   await fetch(`https://c7ds-log.microcms.io/api/v1/articles/${id}`, key)
---
+++ src/types/topImage.ts
@@ -0,0 +1,5 @@
+export type TopImage = {
+  url: string
+  height: number
+  width: number
+}
