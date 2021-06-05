8d11365de58e941cb199e24bdaf7feec471d2d0a
Crea7dosSantos <cr26yusaku0720@icloud.com>
2021-06-06 02:14:10 +0900

    close #39 v2

--- src/components/molecules/Logo.tsx
+++ src/components/molecules/Logo.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import { LogoImage } from '../atoms/LogoImage'
-import { LogoTitle } from '../atoms/LogoTitle'
 
 export const Logo: React.FC = () => {
   return (
-    <div className="flex space-x-3 font-medium">
+    <div className="flex items-center">
       <LogoImage />
-      <div className="mt-1">
-        <LogoTitle />
-      </div>
+      <h1 className="text-2xl ml-2.5 font-bold">C7DS</h1>
     </div>
   )
 }
