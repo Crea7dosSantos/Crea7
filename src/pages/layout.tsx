ccef0bab5c9ab0eac332a069e09d3e4d80c952d9
Crea7dosSantos <cr26yusaku0720@icloud.com>
2021-06-10 22:57:13 +0900

    close #45

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
   return (
     <div>
       <Header />
-      <div className="m-auto w-9/12">{children}</div>
+      <div className="m-auto w-8/12">{children}</div>
     </div>
   )
 }
