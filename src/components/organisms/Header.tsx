5660edad1b02b9532bd59f2374dbc331394572e2
Crea7dosSantos <cr26yusaku0720@icloud.com>
2021-07-08 01:44:48 +0900

    close #48

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -4,7 +4,7 @@ import { Logo } from '../molecules/Logo'
 export const Header: React.FC = () => {
   return (
     <header className="bg-black">
-      <div className="m-auto w-8/12">
+      <div className="m-auto w-11/12 sm:w-11/12 md:w-10/12 lg:w-9/12">
         <Logo />
       </div>
     </header>
