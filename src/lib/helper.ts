a64b9a603419d13766a7060d6dc4b6bfa4b627ac
Crea7dosSantos <cr26yusaku0720@icloud.com>
2021-07-13 15:37:26 +0900

    close #74

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,4 +1,22 @@
 export function getHostFromURL(url: string) {
   const urlObj = new URL(url)
+  if (urlObj.hostname.endsWith('microcms.io')) {
+    return 'microcms.io'
+  }
+
   return urlObj.hostname
+}
+
+export function getHostIconFromURL(url: string) {
+  const urlObj = new URL(url)
+  const hostname = urlObj.hostname
+
+  switch (hostname) {
+    case 'zenn.dev':
+      return '/zenn-favicon.png'
+    case 'qiita.com':
+      return '/qiita-favicon.png'
+    default:
+      return '/micro_cms-favicon.png'
+  }
 }
