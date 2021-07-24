509a35f71914898effd9dd551a32f90d1a1fae03
Crea7dosSantos <cr26yusaku0720@icloud.com>
2021-07-24 22:09:41 +0900

    tiny fix

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -16,19 +16,19 @@ const Layout: NextPage<Props> = ({ children, tags }) => {
       <div className="bg-black text-white">
         <div className="m-auto w-12/11 sm:w-11/12 md:w-10/12 lg:w-11/12 xl:w-9/12 py-12">
           <div className="lg:grid lg:grid-cols-6 lg:gap-10">
-            <div className="col-start-1 col-span-4 sticky">{children}</div>
-            <div className="col-start-5 col-end-7 sticky">
+            <div className="col-start-1 col-span-4">{children}</div>
+            <div className="col-start-5 col-end-7">
               <Profile />
-              <h3 className="text-2xl mb-2 mt-5 text-center">Tags</h3>
-              <p className="">
-                {tags.map((tag) => (
-                  <React.Fragment key={tag.id}>
-                    <TagItem tag={tag} backgroundColor="gray" fullRounded={false} />
-                  </React.Fragment>
-                ))}
-              </p>
             </div>
           </div>
+          <h3 className="text-2xl mb-2 mt-5 text-center">Tags</h3>
+          <p className="">
+            {tags.map((tag) => (
+              <React.Fragment key={tag.id}>
+                <TagItem tag={tag} backgroundColor="gray" fullRounded={false} />
+              </React.Fragment>
+            ))}
+          </p>
         </div>
       </div>
     </div>
