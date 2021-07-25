391556f379fb687b1573f70f4f61c6addc145b12
Crea7dosSantos <cr26yusaku0720@icloud.com>
2021-07-25 11:55:43 +0900

    add responsive point

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -4,7 +4,7 @@ import { Logo } from '../molecules/Logo'
 export const Header: React.VFC = () => {
   return (
     <header className="bg-regal-header-black py-3 border-b border-gray-600">
-      <div className="m-auto sm:w-11/12 md:w-10/12 lg:w-11/12 xl:w-9/12 text-center sm:text-left">
+      <div className="m-auto sm:w-11/12 lg:w-10/12 xl:w-11/12 2xl:w-9/12 text-center sm:text-left">
         <Logo />
       </div>
     </header>
