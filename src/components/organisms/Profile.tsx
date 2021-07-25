25946d61c86f669912ff52b7ce6f2f9185157c10
Crea7dosSantos <cr26yusaku0720@icloud.com>
2021-07-26 02:29:32 +0900

    tiny fix footer layout

--- src/components/organisms/Profile.tsx
+++ src/components/organisms/Profile.tsx
@@ -9,15 +9,15 @@ import { MediumIcon } from '../atoms/MediumIcon'
 export const Profile: React.VFC = () => {
   return (
     <div className="bg-gray-900 border border-gray-800 shadow-lg rounded-3xl p-4 mx-5 sm:mx-0">
-      <div className="sm:flex lg:block">
-        <div className="relative h-32 w-32 mb-3 sm:mb-0 lg:mb-3">
+      <div className="sm:flex xl:block">
+        <div className="relative h-32 w-32 mb-3 sm:mb-0 xl:mb-3">
           <img
             src="https://tailwindcomponents.com/storage/avatars/njkIbPhyZCftc4g9XbMWwVsa7aGVPajYLRXhEeoo.jpg"
             alt="aji"
             className="w-32 h-32 object-cover rounded-2xl"
           />
         </div>
-        <div className="flex-auto sm:ml-5 lg:ml-0 justify-evenly">
+        <div className="flex-auto sm:ml-5 xl:ml-0 justify-evenly">
           <div className="flex items-center justify-between sm:mt-2">
             <div className="flex items-center">
               <div className="flex flex-col">
