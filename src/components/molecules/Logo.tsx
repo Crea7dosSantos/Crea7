a042491ca9c5ddff7d0ef541d2c3d6fa56cb4236
Crea7dosSantos <cr26yusaku0720@icloud.com>
2021-08-11 03:04:14 +0900

    tiny fix

--- src/components/molecules/Logo.tsx
+++ src/components/molecules/Logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export const Logo: React.VFC = () => {
   return (
-    <Link href="/" as={`/index`}>
+    <Link href="/">
       <a className="text-2xl font-medium text-white hover:text-blue-600">Crea7</a>
     </Link>
   )
--- src/components/organisms/TopProfile.tsx
+++ src/components/organisms/TopProfile.tsx
@@ -4,7 +4,7 @@ import { TwitterIcon } from '../atoms/TwitterIcon'
 export const TopProfile: React.VFC = () => {
   return (
     <div className="flex flex-col w-full sticky xl:top-36 xl:w-1/3 mt-2 xl:mt-8 px-8">
-      <p className="ml-2 text-regal-light-gray uppercase tracking-loose">Life Process</p>
+      <p className="ml-2 text-regal-light-gray uppercase tracking-loose">Timeline</p>
       <p className="text-3xl xl:text-4xl leading-normal xl:leading-relaxed mb-2">Crea7 timeline</p>
       <p className="text-sm xl:text-base text-regal-light-gray mb-4">
         web/iOS application developer. This is my history. <br />
@@ -17,12 +17,6 @@ export const TopProfile: React.VFC = () => {
         </a>{' '}
         for daily updates.
       </p>
-      <a
-        href="/"
-        className="bg-transparent mr-auto hover:bg-regal-weight-gray text-regal-light-gray hover:text-white rounded shadow hover:shadow-lg py-2 px-4 border hover:border-transparent"
-      >
-        Explore Now
-      </a>
     </div>
   )
 }
