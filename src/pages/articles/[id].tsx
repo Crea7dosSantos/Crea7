b6b1651a98a92ea1ab6e1ea4e8948ec7f28bf322
Crea7dosSantos <cr26yusaku0720@icloud.com>
2021-09-18 18:21:30 +0900

    change article image scheme

--- src/pages/articles/[id].tsx
+++ src/pages/articles/[id].tsx
@@ -20,7 +20,7 @@ const ArticlePage: NextPage<Props> = ({ article, categories }) => {
       <div className="bg-white shadow-2xl sm:rounded-lg mb-6 tracking-wide">
         <div className="md:flex-shrink-0">
           <img
-            src={article.articleImageUrl}
+            src={article.topImage.url}
             alt="mountains"
             className="w-full h-48 sm:h-56 md:h-64 lg:h-72 xl:h-80 2xl:h-96 sm:rounded-lg sm:rounded-b-none"
           />
