c0a595eeea0456e4ed68afa5008068412751b998
Crea7dosSantos <cr26yusaku0720@icloud.com>
2021-09-18 21:26:04 +0900

    modify blockquote font size

--- next-config.js
+++ next-config.js
@@ -1,5 +1,8 @@
-module.exports = {
-  images: {
-    domains: [],
+// const withPWA = require('next-pwa')
+const withPWA = await import('next-pwa')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
   },
-}
+})
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,36 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { GA_TRACKING_ID } from '../lib/gtag'
 
+const APP_NAME = 'Crea7 Timeline'
+const APP_DESCRIPTION = 'web/iOS application developer. This is my history.'
+
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    return await Document.getInitialProps(ctx)
   }
 
   render() {
     return (
-      <Html lang="ja">
+      <Html lang="ja" dir="ltr">
         <Head>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <meta name="application-name" content={APP_NAME} />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+          <meta name="description" content={APP_DESCRIPTION} />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content="#FFFFFF" />
+          {/* TIP: set viewport head meta tag in _app.js, otherwise it will show a warning */}
+          {/* <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+          /> */}
+          <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+
           <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
           <script
             dangerouslySetInnerHTML={{
